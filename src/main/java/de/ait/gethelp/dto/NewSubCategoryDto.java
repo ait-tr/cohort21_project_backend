281dc645a384f760d623fdc67557ac920553db14
Luzyanin Kirill <jk87@rambler.ru>
2023-06-22 22:01:34 +0300

    added SubCategoryDTO

---
+++ src/main/java/de/ait/gethelp/dto/NewSubCategoryDto.java
@@ -0,0 +1,13 @@
+package de.ait.gethelp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewSubCategoryDto {
+    private String title;
+    private String description;
+}
